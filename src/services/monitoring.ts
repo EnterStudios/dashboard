219cf952231c9d98b91b071547873489cba47a38
Rey César Pier Castañeda Haro <pier_1_2_3@hotmail.com>
2018-04-30 14:38:02 -0500

    https monitor (#691)

--- src/services/monitoring.ts
+++ src/services/monitoring.ts
@@ -17,7 +17,7 @@ namespace MonitoringService {
 
     const BASE_URL = process.env.NODE_ENV === "production" ?
         process.env.MONITOR_API_URL :
-        "http://monitor-api-dev.bespoken.io";
+        "https://monitor-api-dev.bespoken.tools";
 
     export function getUpTimeSummary(query: Query, source: string): Promise < UpTimeSummary[] > {
         let url = BASE_URL + "/sources/" + source + "/pings?" + query.query();
